073fdfb26e52b98fb9d6711dcc917655ad69651d
Zak Keck <Zak.Keck@roguerecruit.com>
2022-04-13 18:34:04 -0500

    Day 1 Finished

--- scripts/tabs.js
+++ scripts/tabs.js
@@ -1,11 +1,11 @@
 window.addEventListener("load", function () {
-  var loader = document.getElementsByClassName("loader");
+  let loader = document.getElementsByClassName("loader");
   loader[0].style.display = "none";
   getTab();
 });
 
 function changeTab(id, tabName) {
-  var i, tabcontent;
+  let i, tabcontent;
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
@@ -13,7 +13,7 @@ function changeTab(id, tabName) {
 
   document.getElementById(tabName).style.display = "block";
 
-  var tabs = document.getElementsByClassName("tab");
+  let tabs = document.getElementsByClassName("tab");
   for (i = 0; i < tabs.length; i++) {
     tabs[i].classList.remove("active");
   }
@@ -23,7 +23,7 @@ function changeTab(id, tabName) {
 }
 
 function getTab() {
-  var tab = JSON.parse(localStorage.getItem("tab"));
+  let tab = JSON.parse(localStorage.getItem("tab"));
   if (tab && tab.id && tab.content) {
     changeTab(tab.id, tab.content);
   } else {
